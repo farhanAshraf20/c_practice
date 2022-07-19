3ae5054f0638e5ac70ca78f32d33ebe691497798
farhanAshraf20 <farhan.ashraf@einfochips.com>
2022-07-19 12:08:04 +0530

    some changes

--- function/00variadic.c
+++ function/00variadic.c
@@ -3,19 +3,19 @@
 
 void print_ints(int num,...)
 {
-    va_list args;
-    va_start(args,num);
-   for(int i=0;i<num;i++)
-    {
-        int value= va_arg(args,int);
-        printf("%d: %d\n",i,value);
-    }
-    va_end(args);
+    va_list args; //pointer for varriable argument list
+    va_start(args,num);
+    for(int i=0;i<num;i++)
+    {
+        int value= va_arg(args,int);
+        printf("%d: %d\n",i,value);
+        }
+        va_end(args);
 }
 int main()
 {
-    print_ints(3,24,25,45);
-    print_ints(3,23,456);
+    print_ints(3,24,25,45);
+    print_ints(3,23,456);
 }
 
 

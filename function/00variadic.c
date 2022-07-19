fd7be6e9b31b2b03fb3d2bc5429a3150846a4082
farhanAshraf20 <farhan.ashraf@einfochips.com>
2022-07-19 11:07:55 +0530

    some update

---
+++ function/00variadic.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include<stdarg.h>
+
+void print_ints(int num,...)
+{
+    va_list args;
+    va_start(args,num);
+   for(int i=0;i<num;i++)
+    {
+        int value= va_arg(args,int);
+        printf("%d: %d\n",i,value);
+    }
+    va_end(args);
+}
+int main()
+{
+    print_ints(3,24,25,45);
+    print_ints(3,23,456);
+}
+
+

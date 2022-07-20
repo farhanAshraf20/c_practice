e536be6eeb832ffe102579524fc58ee55dc6bde8
farhanAshraf20 <farhan.ashraf@einfochips.com>
2022-07-20 11:01:17 +0530

    some update

---
+++ Input_output/input_output1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+    char ch = '\0';
+    ch =getc(stdin);
+    printf("read in character %c \n", ch);
+
+    return 0;
+}
---
+++ Input_output/input_output3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+   int ch = 0;
+   while ((ch = getchar())!=EOF)
+   {
+    printf("%c\n",ch);
+    
+   }
+   
+    return 0;
+}
